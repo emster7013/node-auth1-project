830dd8dd65e240789116b3265cce2ef24fcca321
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-04-22 22:51:05 -0400

    created migrations and added knex file/update package.json

---
+++ data/migrations/20200422224545_auth_table.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('users', users =>{
+      users.increments();
+      users
+      .string('usernmae', 128)
+      .notNullable()
+      .unique();
+      users.string('password', 128).notNullable();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('users');
+};

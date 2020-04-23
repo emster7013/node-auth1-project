d0273a582ea3e9bfc0f53c228747186bc306bcbf
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-04-22 23:33:51 -0400

    starting server.js

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
   
-const express = require('express');
-
+const knexSessionStore = require('connect-session-knex');//(session)
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
+const UserRouter = require('./users/user-router.js');
 const server = express();
 
 server.use(express.json());
+server.use(helmet());
+server.use(cors());
+server.use('/user', UserRouter);
+
 
 module.exports = server;

acccde7f386433d65f67c0e34c99a9023e1a1a30
Emily Gauthier <52601501+emster7013@users.noreply.github.com>
2020-04-24 21:01:48 -0400

    forgot to uncomment a thing in index.js

--- index.js
+++ index.js
@@ -2,4 +2,4 @@
 const server = require('./server.js');
 
 const port = process.env.PORT || 5000;
-// server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
+server.listen(port, () => console.log(`\n** Running on port ${port} **\n`));
